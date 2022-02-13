8ed61da211eb7ca7ea0c8c2c80875b0d3c2a80b8
laurentmontier <laurent.montier@numericable.fr>
2022-02-13 09:10:28 +0100

    sss

--- Test/src/main/java/fr/test/Bonjour.java
+++ Test/src/main/java/fr/test/Bonjour.java
@@ -4,7 +4,7 @@ public class Bonjour {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Bonjour !!");
+		System.out.println("Salut !");
 	}
 
 }
